f7eb2d18148f0a3bd5db0c3cce1de557fad52244
authenticoder <authenticoderio@gmail.com>
2020-06-04 02:09:00 +0300

    add js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,23 +1,55 @@
 function Gallery(gallery) {
+    if(!gallery) {
+        throw new Error('No Gallery Found!');
+    }
 
+    // Select the elements we need
+    const images = Array.from(gallery.querySelectorAll('img'));
+    const modal = document.querySelector('.modal');
+    const prevButton = modal.querySelector('.prev');
+    const nextButton = modal.querySelector('.next');
+    let currentImage;
+
+    function openModal() {
+        console.info('Opening modal...');
+        // First check if the modal is already open
+        if(modal.matches('.open')) {
+            console.info('Modal already open');
+            return;
+        }
+        modal.classList.add('open');
+    }
+
+    function closeModal() {
+        modal.classList.remove('open');
+        // TODO: add event listeners for clicks and keyboard
+    }
+
+    function showImage(imgEl) {
+        if(!imgEl) {
+            console.info('no image to show');
+            return;
+        }
+
+        // update the modal with this info
+        console.log(imgEl);
+        modal.querySelector('img').src = imgEl.src;
+        modal.querySelector('h2').textContent = imgEl.title;
+        modal.querySelector('figure p').textContent = imgEl.dataset.description;
+        currentImage = imgEl;
+        openModal();
+    }
+
+    images.forEach(image => image.addEventListener('click', e => showImage(e.currentTarget)));
 }
+
 
 
 // Use it on the page
 const gallery1 = Gallery(document.querySelector('.gallery1'));
 const gallery2 = Gallery(document.querySelector('.gallery2'));
 
-function wrap(n) {
-    let local = n;
-    function res() {
-        return local;
-    } 
-}
 
-// n = 1 , local = 1, res is returning 1. 
-
-let wrap1 = wrap(1);
-wrap1();
 
 
 // When a return statement is called in a function, the execution of this function is stopped.
